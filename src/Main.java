2691d1b1848cc9c6f61f13380b5ee02b19cba5df
Dinesh Adabala <dinesh.adabala417@gmail.com>
2024-02-20 13:09:22 +0530

    second commit not overrided

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int principalAmount=100000;
+
+        for(int i=1;i<=12;i++){
+            principalAmount=principalAmount-1000;
+            System.out.println("HEmi in "+i+" month "+principalAmount);
+
+        }
+
+
+
+
+
+
     }
 }
